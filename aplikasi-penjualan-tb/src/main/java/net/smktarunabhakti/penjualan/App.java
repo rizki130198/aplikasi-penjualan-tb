4fed5ea5a48bef67467230c7e8832a690fa66e88
rizki130198 <rizkiajja130198@gmail.com>
2014-05-16 17:23:15 +0700

    sudah file yang terbaru

--- aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
+++ aplikasi-penjualan-tb/src/main/java/net/smktarunabhakti/penjualan/App.java
@@ -1,7 +1,11 @@
 package net.smktarunabhakti.penjualan;
 
+import net.smktarunabhakti.penjualan.domain.Barang;
+import net.smktarunabhakti.penjualan.service.AppService;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -9,11 +13,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App 
 {
     private static ApplicationContext appCtx;
+    private static AppService appService;
     
     public static void main( String[] args )
     {
-        appCtx = new ClassPathXmlApplicationContext(
-                "classpath:net/smktarunabhakti/penjualan/"
-                + "applicationContext.xml");         
+        appCtx = new ClassPathXmlApplicationContext("classpath:net/smktarunabhakti/penjualan/"+"applicationContext.xml");
+        
+        appService = (AppService)
+        		appCtx.getBean("appService");
+        saveBarang();
+    }	
+    @SuppressWarnings("unused")
+	private static void saveBarang(){
+    	Barang b = new Barang();    	
+    	b.setKodeBarang("123");
+    	b.setNamaBarang("3Second");
+    	
+    	appService.simpanBarang(b);
+    	
     }
 }
